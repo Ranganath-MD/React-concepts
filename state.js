7a6f5703a0041e263a7ecaaa4d7e86ebbc94cfd1
Ranganath <ranganathmd1995@gmail.com>
2019-11-15 23:29:45 +0530

    state in react

--- state.js
+++ state.js
@@ -1,5 +1,7 @@
 // State is an object which holds some information about the parent component that may change over the life time of the component
 // state only be used in the class component
+//state is used for only an internal communication inside a component
+// Fully controlled by the class component
 
 import React, { Component } from 'react';
 
@@ -7,6 +9,7 @@ class Greeting extends React.Component{
     state = {
         msg : "Hello React!"
     }
+
     render(){
         return (
             <div>{this.state.msg}</div>
